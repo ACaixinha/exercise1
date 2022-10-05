0ff975dc973cffe24f60154584af264a1b262b3e
Anibal Caixinha <anibal.caixinha@advarra.com>
2022-10-05 10:34:16 +0100

    Add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: 'User'
+
+  validates :amount_available, :cost, :product_name, presence: true
+  validate :cost_is_a_multiple_of_five
+
+  private
+  def cost_is_a_multiple_of_five
+    errors.add(:cost, 'Must be a multiple of 5') if cost.nil? || cost.remainder(5) != 0
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,43 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validate amount_available presence' do
+    product = Product.new
+    refute product.valid?
+    assert_not_nil product.errors[:amount_available]
+  end
+
+  test 'validate cost presence' do
+    product = Product.new
+    refute product.valid?
+    assert_not_nil product.errors[:cost]
+  end
+
+  test 'validate product_name presence' do
+    product = Product.new
+    refute product.valid?
+    assert_not_nil product.errors[:product_name]
+  end
+
+  test 'validate seller presence' do
+    product = Product.new
+    refute product.valid?
+    assert_not_nil product.errors[:seller]
+  end
+
+  test 'validate cost is invalid when not multiple of 5' do
+    [6, 8, 18, 23, 30, 42, 101].each do |cost|
+      product = Product.new(cost: cost)
+      refute product.valid?
+      assert_not_nil product.errors[:cost]
+    end
+  end
+
+  test 'validate cost is valid when a multiple of 5' do
+    [5, 10, 15, 20, 30, 45, 50].each do |cost|
+      product = Product.new(cost: cost)
+      refute product.valid?
+      assert [], product.errors[:cost]
+    end
+  end
 end
